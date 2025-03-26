c39fe71819d6761945505be9ddf6592b521a7612
jordan <jordan@butterwick.tech>
2025-03-26 10:56:58 -0600

    updates

---
+++ .bashrc
@@ -0,0 +1,66 @@
+# .bashrc
+
+# Source global definitions
+if [ -f /etc/bashrc ]; then
+    . /etc/bashrc
+fi
+
+# User specific environment
+if ! [[ "$PATH" =~ "$HOME/.local/bin:$HOME/bin:" ]]; then
+    PATH="$HOME/.local/bin:$HOME/bin:$PATH"
+fi
+export PATH
+
+# Uncomment the following line if you don't like systemctl's auto-paging feature:
+# export SYSTEMD_PAGER=
+
+# User specific aliases and functions
+if [ -d ~/.bashrc.d ]; then
+    for rc in ~/.bashrc.d/*; do
+        if [ -f "$rc" ]; then
+            . "$rc"
+        fi
+    done
+fi
+unset rc
+
+alias clippy="cargo clippy --all-features -- -D warnings -W clippy::pedantic -W clippy::nursery -W rust-2018-idioms"
+
+alias ll="exa -alh --git --color=always"
+
+# git repository greeter
+last_repository=
+check_directory_for_new_repository() {
+ current_repository=$(git rev-parse --show-toplevel 2> /dev/null)
+ 
+ if [ "$current_repository" ] && \
+    [ "$current_repository" != "$last_repository" ]; then
+  onefetch
+ fi
+ last_repository=$current_repository
+}
+cd() {
+ builtin cd "$@"
+ check_directory_for_new_repository
+}
+
+# optional, greet also when opening shell directly in repository directory
+# adds time to startup
+check_directory_for_new_repository
+
+. "$HOME/.cargo/env"
+
+eval "$(/home/jordan/.local/bin/mise activate bash)"
+eval "$(starship init bash)"
+
+if [[ -z "$ZELLIJ" ]]; then
+    if [[ "$ZELLIJ_AUTO_ATTACH" == "true" ]]; then
+        zellij attach -c
+    else
+        zellij
+    fi
+
+    if [[ "$ZELLIJ_AUTO_EXIT" == "true" ]]; then
+        exit
+    fi
+fi
--- init.sh
+++ init.sh
@@ -1,18 +1,43 @@
 dnf -y install cmake freetype-devel fontconfig-devel libxcb-devel libxkbcommon-devel g++ openssl-devel sqlite-devel python3-tkinter ncurses-devel perl helix
-dnf -y groupinstall "Development Tools"
+dnf -y group install "Development Tools"
+
+dnf copr enable varlad/onefetch
+dnf -y install onefetch
 
 # rust and rust packages
 if ! cargo --version; then
   curl --proto '=https' --tlsv1.2 -sSf https://sh.rustup.rs | sh
 fi
 
-cargo install zellij btm starship rtx-cli bat exa ripgrep
+cargo install zellij bottom rtx-cli bat exa ripgrep
+
+cp -a ./dotfiles/. ~/.config
+
 bat cache --build
 dnf -y install dnf-plugins-core
 dnf config-manager --add-repo https://download.docker.com/linux/fedora/docker-ce.repo
 
 dnf -y install docker-ce docker-ce-cli containerd.io docker-buildx-plugin docker-compose-plugin
 
+curl https://mise.run | sh
+echo "eval \"\$(/home/jordan/.local/bin/mise activate bash)\"" >> ~/.bashrc
+eval "$(mise activate bash)"
+
+curl -sS https://starship.rs/install.sh | sh
+echo "eval "$(starship init bash)"" >> ~/.bashrc
+echo "zellij setup --generate-auto-start bash" >> ~/.bashrc
+
+wget -P ~/Downloads https://github.com/ryanoasis/nerd-fonts/releases/download/v3.3.0/FiraCode.zip
+unzip ~/Downloads/FiraCode.zip
+
+if [ ! -d "/usr/local/share/fonts/FiraCode" ]; then
+  mkdir -p /usr/local/share/fonts/
+  cp -r FiraCode /usr/local/share/fonts/
+  chown -R root: /usr/local/share/fonts/FiraCode/
+  chmod 644 /usr/local/share/fonts/FiraCode/*
+  restorecon -vFr /usr/local/share/fonts/FiraCode/
+  fc-cache -v
+fi
 
 mkdir ~/projects ~/dependencies
 
